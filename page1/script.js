1d71f41ce178e0444d2904613aac21089931b2e0
Temirlan Kazhigerey <ynwredd@mail.ru>
2022-12-17 15:32:37 +0600

    new page2
    added slidebar menu , slider

--- page1/script.js
+++ page1/script.js
@@ -5,16 +5,20 @@ $(document).ready(function(){
     
   });
 
-
-
-
   $("#click2").click(function(){
     $("#slide-panel2").slideToggle("slow");
     $("#plus2").toggleClass('transform');
   });
 
+  });
+function openNav() {
+  document.getElementById("mySidebar").style.width = "150px";
+  document.getElementById("main").style.marginLeft = "150px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
 
 
-
-
-});
