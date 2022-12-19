6b948cd109445ce99bcccbdda7c446eff427e10f
Temirlan Kazhigerey <ynwredd@mail.ru>
2022-12-20 03:44:47 +0600

    changes

--- page1Temirlan/script.js
+++ page1Temirlan/script.js
@@ -1,24 +1,17 @@
 $(document).ready(function(){
-  $("#click").click(function(){
-    $("#slide-panel").slideToggle("slow");
-    $("#plus").toggleClass('transform');
-    
+ 
+  $(".header-burger").click(function (event) {
+    $(".header-burger, .header-menu").toggleClass("active");
+    $("body").toggleClass("lock");
   });
 
-  $("#click2").click(function(){
-    $("#slide-panel2").slideToggle("slow");
-    $("#plus2").toggleClass('transform');
-  });
 
   });
-function openNav() {
-  document.getElementById("mySidebar").style.width = "150px";
-  document.getElementById("main").style.marginLeft = "150px";
-}
 
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 0) {
+    document.getElementsByClassName("header").style.backgroundColor = "white";
+  }
+  
 
 
